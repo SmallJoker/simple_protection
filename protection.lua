3aa1b87eb1a076a2e725afe9e68e62afb6a902c5
aka Krock <st.rentsch@hotmail.com>
2014-05-11 20:37:03 +0200

    Is it already in use?

--- protection.lua
+++ protection.lua
@@ -92,6 +92,10 @@ minetest.register_craftitem("simple_protection:claim", {
 			return
 		end
 		local player_name = user:get_player_name()
+		if simple_protection.old_is_protected(pos, player_name) then
+			minetest.chat_send_player(player_name, "Area is already protected by an other protection mod.")
+			return
+		end
 		local pos = simple_protection.get_location(pointed_thing.under)
 		local data = simple_protection.claims[pos]
 		if data then
